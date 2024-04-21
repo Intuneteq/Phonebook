9635e8ee1187d979298b5b54020bac2bf8faf2c8
Tobi Olanitori <to21abo@herts.ac.uk>
2024-04-21 13:37:28 +0100

    test: unit test to duplicate add functionality

--- src/test/java/Data/PhoneBkDataTest.java
+++ src/test/java/Data/PhoneBkDataTest.java
@@ -1,17 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
- */
 package Data;
 
-import java.beans.PropertyChangeListener;
-import java.util.ArrayList;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
+//import java.beans.PropertyChangeListener;
+//import java.util.ArrayList;
+//import org.junit.jupiter.api.AfterAll;
+//import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+//import static org.junit.jupiter.api.Assertions.fail;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.BeforeEach;
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -19,130 +18,155 @@ import org.junit.jupiter.api.Test;
  * @author steve
  */
 public class PhoneBkDataTest {
-    
-    public PhoneBkDataTest() {
-    }
 
-    @BeforeAll
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterAll
-    public static void tearDownClass() throws Exception {
-    }
-
-    @BeforeEach
-    public void setUp() throws Exception {
-    }
-
-    @AfterEach
-    public void tearDown() throws Exception {
-    }
-    
-    
-
-    
-
-    /**
-     * Test of addPropertyChangeListener method, of class PhoneBookData.
-     */
+//    public PhoneBkDataTest() {
+//    }
+//
+//    @BeforeAll
+//    public static void setUpClass() throws Exception {
+//    }
+//
+//    @AfterAll
+//    public static void tearDownClass() throws Exception {
+//    }
+//
+//    @BeforeEach
+//    public void setUp() throws Exception {
+//    }
+//
+//    @AfterEach
+//    public void tearDown() throws Exception {
+//    }
+//
+//    /**
+//     * Test of addPropertyChangeListener method, of class PhoneBookData.
+//     */
+//    @Test
+//    public void testAddPropertyChangeListener() {
+//        System.out.println("addPropertyChangeListener");
+//        PropertyChangeListener outsidePcl = null;
+//        PhoneBkData instance = new PhoneBkData();
+//        instance.addPropertyChangeListener(outsidePcl);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of addContact method, of class PhoneBookData.
+//     */
+//    @Test
+//    public void testAddContact() {
+//        System.out.println("addContact");
+//        String _contactName = "";
+//        String _contactNumber = "";
+//        PhoneBkData instance = new PhoneBkData();
+//        boolean expResult = false;
+//        boolean result = instance.addContact(_contactName, _contactNumber);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of getSize method, of class PhoneBookData.
+//     */
+//    @Test
+//    public void testGetSize() {
+//        System.out.println("getSize");
+//        PhoneBkData instance = new PhoneBkData();
+//        int expResult = 0;
+//        int result = instance.getSize();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of findContactAt method, of class PhoneBookData.
+//     */
+//    @Test
+//    public void testFindContactAt() {
+//        System.out.println("findContactAt");
+//        String _contactName = "";
+//        String _contactNumber = "";
+//        PhoneBkData instance = new PhoneBkData();
+//        int expResult = 0;
+//        int result = instance.findContactAt(_contactName, _contactNumber);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of deleteContact method, of class PhoneBookData.
+//     */
+//    @Test
+//    public void testDeleteContact() {
+//        System.out.println("deleteContact");
+//        String _contactName = "";
+//        String _contactNumber = "";
+//        PhoneBkData instance = new PhoneBkData();
+//        boolean expResult = false;
+//        boolean result = instance.deleteContact(_contactName, _contactNumber);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of getContactList method, of class PhoneBookData.
+//     */
+//    @Test
+//    public void testGetContactList() {
+//        System.out.println("getContactList");
+//        PhoneBkData instance = new PhoneBkData();
+//        ArrayList<PhoneBkItem> expResult = null;
+//        ArrayList<PhoneBkItem> result = instance.getContactList();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of setContactList method, of class PhoneBookData.
+//     */
+//    @Test
+//    public void testSetContactList() {
+//        System.out.println("setContactList");
+//        ArrayList<PhoneBkItem> contactList = null;
+//        PhoneBkData instance = new PhoneBkData();
+//        instance.setContactList(contactList);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
     @Test
-    public void testAddPropertyChangeListener() {
-        System.out.println("addPropertyChangeListener");
-        PropertyChangeListener outsidePcl = null;
+    public void DontAllowDuplicateNames() {
+        System.out.println("DontAllowDuplicateNames");
+
+        String contactName = "Tester";
+
         PhoneBkData instance = new PhoneBkData();
-        instance.addPropertyChangeListener(outsidePcl);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
+        // Add first item
+        boolean isAddContact1 = instance.addContact(contactName, "12345678");
+
+        // Assert first name added
+        assertTrue(isAddContact1);
+
+        ArrayList<PhoneBkItem> list = instance.getContactList();
+
+        // The first item should be isAddContact1
+        PhoneBkItem item1 = list.getFirst();
+
+        // Assert name is actually in the list
+        assertEquals(contactName, item1.getContactName());
+
+        // Attempt to add second contact item with same name
+        boolean isAddContact2 = instance.addContact(contactName, "12345679");
+
+        // assert second name added
+        assertFalse(isAddContact2);
+
     }
 
-    /**
-     * Test of addContact method, of class PhoneBookData.
-     */
-    @Test
-    public void testAddContact() {
-        System.out.println("addContact");
-        String _contactName = "";
-        String _contactNumber = "";
-        PhoneBkData instance = new PhoneBkData();
-        boolean expResult = false;
-        boolean result = instance.addContact(_contactName, _contactNumber);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getSize method, of class PhoneBookData.
-     */
-    @Test
-    public void testGetSize() {
-        System.out.println("getSize");
-        PhoneBkData instance = new PhoneBkData();
-        int expResult = 0;
-        int result = instance.getSize();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of findContactAt method, of class PhoneBookData.
-     */
-    @Test
-    public void testFindContactAt() {
-        System.out.println("findContactAt");
-        String _contactName = "";
-        String _contactNumber = "";
-        PhoneBkData instance = new PhoneBkData();
-        int expResult = 0;
-        int result = instance.findContactAt(_contactName, _contactNumber);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of deleteContact method, of class PhoneBookData.
-     */
-    @Test
-    public void testDeleteContact() {
-        System.out.println("deleteContact");
-        String _contactName = "";
-        String _contactNumber = "";
-        PhoneBkData instance = new PhoneBkData();
-        boolean expResult = false;
-        boolean result = instance.deleteContact(_contactName, _contactNumber);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getContactList method, of class PhoneBookData.
-     */
-    @Test
-    public void testGetContactList() {
-        System.out.println("getContactList");
-        PhoneBkData instance = new PhoneBkData();
-        ArrayList<PhoneBkItem> expResult = null;
-        ArrayList<PhoneBkItem> result = instance.getContactList();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setContactList method, of class PhoneBookData.
-     */
-    @Test
-    public void testSetContactList() {
-        System.out.println("setContactList");
-        ArrayList<PhoneBkItem> contactList = null;
-        PhoneBkData instance = new PhoneBkData();
-        instance.setContactList(contactList);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
 }
--- src/test/java/Data/PhoneBkItemTest.java
+++ src/test/java/Data/PhoneBkItemTest.java
@@ -1,120 +1,91 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
- */
+
 package Data;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.AfterAll;
+//import org.junit.jupiter.api.AfterEach;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.fail;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
 
 /**
  *
  * @author steve
  */
 public class PhoneBkItemTest {
-    
-    public PhoneBkItemTest() {
-    }
 
-    @BeforeAll
-    public static void setUpClass() throws Exception {
-    }
+//    /**
+//     * Test of getContactName method, of class PhoneBookItem.
+//     */
+//    @Test
+//    public void testGetContactName() {
+//        System.out.println("getContactName");
+//        PhoneBkItem instance = new PhoneBkItem("", "");
+//        String expResult = "";
+//        String result = instance.getContactName();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of equals method, of class PhoneBookItem.
+//     */
+//    @Test
+//    public void testEquals() {
+//        System.out.println("equals");
+//        String _contactName = "";
+//        String _contactNumber = "";
+//        PhoneBkItem instance = new PhoneBkItem("", "");
+//        boolean expResult = false;
+//        boolean result = instance.equals(_contactName, _contactNumber);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of setContactName method, of class PhoneBookItem.
+//     */
+//    /**
+//     * Test of getContactNumber method, of class PhoneBookItem.
+//     */
+//    @Test
+//    public void testGetContactNumber() {
+//        System.out.println("getContactNumber");
+//        PhoneBkItem instance = new PhoneBkItem("", "");
+//        String expResult = "";
+//        String result = instance.getContactNumber();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of setContactName method, of class PhoneBookItem.
+//     */
+//    @Test
+//    public void testSetContactName() {
+//        System.out.println("setContactName");
+//        String ContactName = "";
+//        PhoneBkItem instance = new PhoneBkItem("", "");
+//        instance.setContactName(ContactName);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of setContactNumber method, of class PhoneBookItem.
+//     */
+//    @Test
+//    public void testSetContactNumber() {
+//        System.out.println("setContactNumber");
+//        String ContactNumber = "";
+//        PhoneBkItem instance = new PhoneBkItem("", "");
+//        instance.setContactNumber(ContactNumber);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
 
-    @AfterAll
-    public static void tearDownClass() throws Exception {
-    }
-
-    @BeforeEach
-    public void setUp() throws Exception {
-    }
-
-    @AfterEach
-    public void tearDown() throws Exception {
-    }
-    
-    
-
-    /**
-     * Test of getContactName method, of class PhoneBookItem.
-     */
-    @Test
-    public void testGetContactName() {
-        System.out.println("getContactName");
-        PhoneBkItem instance = new PhoneBkItem("","");
-        String expResult = "";
-        String result = instance.getContactName();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of equals method, of class PhoneBookItem.
-     */
-    @Test
-    public void testEquals() {
-        System.out.println("equals");
-        String _contactName = "";
-        String _contactNumber = "";
-        PhoneBkItem instance = new PhoneBkItem("","");;
-        boolean expResult = false;
-        boolean result = instance.equals(_contactName, _contactNumber);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setContactName method, of class PhoneBookItem.
-     */
-   
-
-    /**
-     * Test of getContactNumber method, of class PhoneBookItem.
-     */
-    @Test
-    public void testGetContactNumber() {
-        System.out.println("getContactNumber");
-        PhoneBkItem instance = new PhoneBkItem("","");;
-        String expResult = "";
-        String result = instance.getContactNumber();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setContactName method, of class PhoneBookItem.
-     */
-    @Test
-    public void testSetContactName() {
-        System.out.println("setContactName");
-        String ContactName = "";
-        PhoneBkItem instance = new PhoneBkItem("","");;
-        instance.setContactName(ContactName);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setContactNumber method, of class PhoneBookItem.
-     */
-    @Test
-    public void testSetContactNumber() {
-        System.out.println("setContactNumber");
-        String ContactNumber = "";
-        PhoneBkItem instance = new PhoneBkItem("","");;
-        instance.setContactNumber(ContactNumber);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    
-   
-    
 }
